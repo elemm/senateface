aaa6e322852128659226d837447efdf7f7329097
Emily Lemmerman <eglem@stanford.edu>
2017-05-07 23:29:07 -0700

    make it better

--- app.py
+++ app.py
@@ -2,30 +2,22 @@
 # https://dashboard.heroku.com/apps/dandan1111/deploy/heroku-git
 # https://github.com/datademofun/heroku-basic-flask
 
-
+import csv
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Hello World to my <strong>homepage</strong>!"
+    legislators = list(csv.DictReader(lines))
+    legislators[index]
+    <img src="https://theunitedstates.io/images/congress/225x275/{id}.jpg" alt="image">
+    <a href="senateface.herokuapp.com/{id}">  Senator {name}
+    </a>
 
-@app.route("/hello/<name>")
-def hello(name):
-    return "Hello, <strong>{}!</strong>".format(name)
 
-@app.route("/cage/<width>/<height>")
-def placecage(width=400, height=400):
-    templatetext = """
-    <p>
-        This is a hotlinked {w}x{h} picture of Nicolas Cage
-        from
-        <a href="http//placecage.com">placecage.com</a>:
-    </p>
-    <img src="http://placecage.com/{w}/{h}" alt="placecage">
-    """
-
-    return templatetext.format(w=width, h=height)
+@app.route("/<id>")
+def redirect(id):
+ return <img src="https://theunitedstates.io/images/congress/450x550/{id}.jpg" alt="image">
 
 
 
