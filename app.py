4ec62c787b882a8c0513dcd7d5163489354cddf4
Emily Lemmerman <eglem@stanford.edu>
2017-05-08 10:46:40 -0700

    hello

--- app.py
+++ app.py
@@ -4,21 +4,31 @@
 
 import csv
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+
+def get_csv():
+        csv_path = './static/115th congress spreadsheet - original-sunlight.csv'
+        csv_file = open(csv_path, 'rb')
+        csv_obj = csv.DictReader(csv_file)
+        csv_list = list(csv_obj)
+        return csv_list
+
 
 @app.route("/")
 def homepage():
-    legislators = list(csv.DictReader(lines))
-    legislators[index]
-    <img src="https://theunitedstates.io/images/congress/225x275/{id}.jpg" alt="image">
-    <a href="senateface.herokuapp.com/{id}">  Senator {name}
-    </a>
+    template = 'index.html'
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
 
 
-@app.route("/<id>")
-def redirect(id):
- return <img src="https://theunitedstates.io/images/congress/450x550/{id}.jpg" alt="image">
+#@app.route("/<id>")
 
+#<img src="https://theunitedstates.io/images/congress/225x275/{id}.jpg" alt="image">
+
+
+#def redirect(id):
+ #return <img src="https://theunitedstates.io/images/congress/450x550/{id}.jpg" alt="image">
 
 
 if __name__ == "__main__":
