debba909778100425a51f6285e333b5ff0c60ee7
Emily Lemmerman <eglem@stanford.edu>
2017-05-08 20:11:50 -0700

    need to still add borders

--- app.py
+++ app.py
@@ -9,27 +9,41 @@ app = Flask(__name__)
 
 def get_csv():
         csv_path = './static/115th congress spreadsheet - original-sunlight.csv'
-        csv_file = open(csv_path, 'rb')
+        csv_file = open(csv_path, 'r') ## this means open as regular text not as "raw bytes"
         csv_obj = csv.DictReader(csv_file)
         csv_list = list(csv_obj)
         return csv_list
 
+def get_house():
+    rows = get_csv()
+    houseppl = []
+
+    for r in rows:
+        if r['in_office'] == '1' and r['title'] == 'Rep':
+            houseppl.append(r)
+    return houseppl
+
+
+def get_gender(gender):
+    return [m for m in get_house() if m['gender'] == gender]
+
+
 
 @app.route("/")
 def homepage():
-    template = 'index.html'
-    object_list = get_csv()
-    return render_template(template, object_list=object_list)
+    congress = get_house()
+    return render_template('index.html',congress=congress)
 
 
-#@app.route("/<id>")
+@app.route("/table")
+def tablepage():
+    return render_template('table.html', object_list=get_csv())
 
-#<img src="https://theunitedstates.io/images/congress/225x275/{id}.jpg" alt="image">
 
-
-#def redirect(id):
- #return <img src="https://theunitedstates.io/images/congress/450x550/{id}.jpg" alt="image">
+@app.route("/<id>")
+def redirect(id):
+    return render_template('reppage.html',rep_id=id,congress=get_house())
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, use_reloader=True)
